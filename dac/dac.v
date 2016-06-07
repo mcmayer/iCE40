89662862e73a21588b4b40d6bcee590a3879c5f8
MM <mmayer@mayeranalytics.com>
2016-06-07 13:07:07 +0200

    update to dac/

--- dac/dac.v
+++ dac/dac.v
@@ -1,13 +1,13 @@
-‘timescale 100 ps / 10 ps
+//`timescale 100 ps / 10 ps
 
 //This is a Delta-Sigma Digital to Analog Converter
 // adapted from Xilinx App Note XAPP154 (September 23, 1999)
 // http://www.xilinx.com/support/documentation/application_notes/xapp154.pdf
 module dac(
-    input CLK_i;
-    input RSTn_i;
-    output DACout; // This is the average output that feeds low pass filter
-    input [MSBI:0] DACin;  // DAC input (excess 2**MSBI)
+    input CLK_i,
+    input RSTn_i,
+    output DACout, // This is the average output that feeds low pass filter
+    input [MSBI:0] DACin  // DAC input (excess 2**MSBI)
 );
 
 parameter MSBI = 7;  // Most significant Bit of DAC input
@@ -20,25 +20,25 @@ reg [MSBI+2:0] DeltaB;     // B input of Delta adder
 
 
 always @(SigmaLatch)
-    DeltaB = {SigmaLatch[MSBI+2], SigmaLatch[MSBI+2]} << (MSBI+1);
+    DeltaB <= {SigmaLatch[MSBI+2], SigmaLatch[MSBI+2]} << (MSBI+1);
 
 always @(DACin or DeltaB)
-    DeltaAdder = DACin + DeltaB;
+    DeltaAdder <= DACin + DeltaB;
 
 always @(DeltaAdder or SigmaLatch)
-    SigmaAdder = DeltaAdder + SigmaLatch;
+    SigmaAdder <= DeltaAdder + SigmaLatch;
 
 always @(posedge CLK_i or negedge RSTn_i)
 begin
      if(~RSTn_i)
      begin
-           SigmaLatch <= #1 1’bl << (MSBI+1);
-           DACout <= #1 1‘b0;
+           SigmaLatch <= 1'b1 << (MSBI+1);
+           DACout <= 1'b0;
      end
      else
      begin
-        SigmaLatch <== #1 SigmaAdder;
-        DACout <= #1 SigmaLatch[MSBI+2];
+        SigmaLatch <= SigmaAdder;
+        DACout <= SigmaLatch[MSBI+2];
     end
 end
 
