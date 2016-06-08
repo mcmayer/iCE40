f3385bcf1f0dd57f38a7c93a60373176c7c2be0f
MM <mmayer@mayeranalytics.com>
2016-06-08 21:00:34 +0200

    shift LED left/right instead of counting

--- rotary_enc/rotary_enc_top.v
+++ rotary_enc/rotary_enc_top.v
@@ -1,13 +1,13 @@
 `include "rotary_enc.v"
 
 //
-//         ------------------
-//    GND -|              A |- D14
-//         |                |
-//         |                |- GND
-//         |                |
-//    B16 -| BTN          B |- D15
-//         ------------------
+//          ------------------
+//    GND --|              A |-- D14
+//          |     ROTARY     |
+//          |     ENCODER    |-- GND
+//          |                |
+//    B16 --| BTN          B |-- D15
+//          ------------------
 //
 
 module rotary_enc_top (
@@ -19,22 +19,28 @@ module rotary_enc_top (
     output [7:0] LED_o
     );
 
-    reg [7:0] counter = 0;
     wire rot_dir, rot_event;
-
-    wire left, right;
 
     rotary_decoder my_rotary_decoder(
         .CLK_i(CLK_i),
-        .rot_1_i(D14_i),
-        .rot_2_i(D15_i),
+        .rot_1_i(~D14_i),
+        .rot_2_i(~D15_i),
         .rot_dir_o(rot_dir),
         .rot_event_o(rot_event)
         );
 
+
+    // shift a bit left and right depending on rot_dir
+
+    reg [7:0] counter = 0;
+
     always @(posedge CLK_i) begin
-        if(rot_event)
-            counter <= rot_dir ? counter+1 : counter-1;
+        if(rot_event) begin
+            if(counter == 0)
+                counter <= rot_dir ? 8'b00000001 : 8'b10000000;
+            else
+                counter <= rot_dir ? counter<<1 : counter>>1;
+        end
         if(~RSTn_i | ~B16_i)
             counter <= 0;
     end
