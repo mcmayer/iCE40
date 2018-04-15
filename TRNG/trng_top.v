da066db00de588218d3d9ef0cc7e2aec3f6bfbec
MM <mmayer@mayeranalytics.com>
2018-04-15 13:13:23 +0200

    sr

--- TRNG/trng_top.v
+++ TRNG/trng_top.v
@@ -2,18 +2,29 @@ module trng_top(
     input CLK_i,
     output PMOD_1,
     output PMOD_2,
+    output PMOD_3,
     output [4:0]LED_o
 );
 
+    wire q    /* synthesis syn_keep = 1 */;
+    wire qbar    /* synthesis syn_keep = 1 */;
+    reg r /* synthesis syn_keep = 1 */;
+
     assign PMOD_1 = CLK_i;
-    assign LED_o[3] = CLK_i;
-    assign LED_o[2:0] = 0;
+    assign PMOD_2 = q;
+    assign PMOD_3 = qbar;
+    assign LED_o[0] = q;
+    assign LED_o[1] = CLK_i;
+    assign LED_o[2] = r;
+    assign LED_o[4:3] = 0;
 
-    assign LED_o[4] = out;
-    assign PMOD_2 = out;
+    SR_latch sr(CLK_i, CLK_i, q, qbar) /* synthesis syn_keep = 1 */;
 
-    wire out;
-
-    flip_flop ff(CLK_i, out);
+    always @(posedge CLK_i)
+    begin
+      if (q == 1'b1) begin
+        r <= 1'b1;
+      end
+    end
 
 endmodule
